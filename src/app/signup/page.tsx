fd66a97c8aa6466b64406c39eb819486fd164e0d
John Doe <ka7de0112@gmail.com>
2024-08-16 22:37:40 +0900

    11章修正

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,6 +2,8 @@
 
 import { supabase } from '@/utils/supabase';
 import { useState } from 'react';
+import { Button } from '@/app/_components/Button';
+import { Input } from '@/app/_components/input';
 
 export default function Page() {
   const [email, setEmail] = useState('');
@@ -31,14 +33,17 @@ export default function Page() {
       <form onSubmit={handleSubmit} className='space-y-4 w-full max-w-[400px]'>
         <div>
           <label htmlFor="email" className='block mb-2 text-sm font-medium text-gray-900'>メールアドレス</label>
-          <input type="email" name='email' id='email' className='bg-gray-50 border border-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5' placeholder='name@company' required onChange={(e) => setEmail(e.target.value)} value={email} />
+          <Input type={email} name={email} id={email} placeholder={'name@company'} onChange={(e) => setEmail(e.target.value)} value={email} />
+          {/* <input type="email" name='email' id='email' className='bg-gray-50 border border-gray-900 text-sm rounded-lg focus:border-blue-500 block w-full p-2.5' placeholder='name@company' required onChange={(e) => setEmail(e.target.value)} value={email} /> */}
         </div>
         <div>
           <label htmlFor="password" className='block mb-2 font-medium text-gray-900'>パスワード</label>
-          <input type="password" name='password' id='password' placeholder='・・・・・・・' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' required onChange={(e) => setPassword(e.target.value)} value={password} />
+          <Input type={password} name={password} id={password} placeholder={'・・・・・・・'} onChange={(e) => setPassword(e.target.value)} value={password} />
+          {/* <input type="password" name='password' id='password' placeholder='・・・・・・・' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' required onChange={(e) => setPassword(e.target.value)} value={password} /> */}
         </div>
 
         <div>
+          <Button menu={'登録'} />
           <button type='submit' className='w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>登録</button>
         </div>
       </form>
